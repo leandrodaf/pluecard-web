2cf9676fb060aa732204a8d98980ec6b21150a89
Leandro Ferreira <leandro.daf4@gmail.com>
2021-03-12 16:19:07 -0300

    fix middleware

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -1,7 +1,7 @@
 export default function auth({ next, router, store }) {
 
     if (!store.getters.isAuth) {
-        return router.push({ name: 'Login' }).catch(() => { });
+        return storestore.dispatch("logOut").then(() => router.push({ name: 'Login' }).catch(() => { }));
     }
 
     return next();
